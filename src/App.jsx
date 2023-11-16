00a07a7482bee0db645f63dd2c27416394980437
gustavodesousalima <gustavodesousalima918@gmail.com>
2023-11-16 15:55:15 -0300

    Alter files project

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import LoginPage from './pages/login/LoginPage';
 import FeedPage from './pages/feed/FeedPage';
 import { AuthProvider } from './context/authContext';
@@ -8,12 +8,12 @@ import './App.css';
 function App() {
   return (
     <AuthProvider>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path={ROUTES.LOGIN} element={<LoginPage />} />
           <Route path={ROUTES.FEED} element={<FeedPage />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </AuthProvider>
   );
 }
