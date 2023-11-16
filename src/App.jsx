db848197e1af2864b92430b1ce369e1efc4e5b1f
gustavodesousalima <gustavodesousalima918@gmail.com>
2023-11-16 14:13:36 -0300

    Alter files project

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import LoginPage from './pages/login/LoginPage'
-import './App.css'
+import LoginPage from './pages/login/LoginPage';
+import { AuthProvider } from './context/authContext';
+import './App.css';
 
 function App() {
   return (
-    <>
+    <AuthProvider>
       <LoginPage />
-    </>
-  )
+    </AuthProvider>
+  );
 }
 
-export default App
+export default App;
