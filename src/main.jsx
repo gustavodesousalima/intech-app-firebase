6113640ec73dde3389e52447b82eb0bdb3ed2a1e
gustavodesousalima <gustavodesousalima918@gmail.com>
2023-11-16 13:42:46 -0300

    Alter files project

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App.jsx';
-import './index.css';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import LoginPage from './pages/login/LoginPage.jsx';
-
-const router = createBrowserRouter([
-  {
-    path: "/appintech/*",  // Update path to include wildcard for child routes
-    element: <App />,
-    children: [
-      {
-        index: true,  // Use index: true for the default child route
-        element: <LoginPage />
-      },
-    ]
-  },
-]);
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-    {/* Remove redundant <App /> here */}
+    <App />
   </React.StrictMode>,
-);
+)
