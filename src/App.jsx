a143a8e04c372a74d29b5f8e6e57462f1eff1e23
gustavodesousalima <gustavodesousalima918@gmail.com>
2023-11-18 01:06:37 -0300

    Alteration BrowserRouter for HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,14 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import LoginPage from './pages/login/LoginPage';
 import FeedPage from './pages/feed/FeedPage';
-import { AuthProvider } from './context/authContext';
 import { ROUTES } from './constants/routes'
 
 function App() {
   return (
-    <AuthProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path={ROUTES.LOGIN} element={<LoginPage />} />
-          <Route path={ROUTES.FEED} element={<FeedPage />} />
-        </Routes>
-      </BrowserRouter>
-    </AuthProvider>
+      <Routes>
+        <Route path={ROUTES.LOGIN} element={<LoginPage />} />
+        <Route path={ROUTES.FEED} element={<FeedPage />} />
+      </Routes>
   );
 }
 
